c486d863d967ee573e1df36a115fb0fa58a211b0
link-short <dora.family228@gmail.com>
2025-10-26 23:27:38 +0500

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -46,7 +46,7 @@ xhr({
 		
 		if (hash) {
 			// Redirect
-			var url = hash.indexOf('http') == 0? hash : 'http://dabblet.com/gist/' + hash;
+			var url = hash.indexOf('http') == 0? hash : 'http://link-short.github.io/' + hash;
 			$('section.redirecting > p').innerHTML = 'Redirecting to <a href="' + url + '">' + url + '</a>…';
 			location.href = url;
 		}
