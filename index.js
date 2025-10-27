d709bc0c55fbbe29d6bd73c043cbc849f2cfa91f
link-short <dora.family228@gmail.com>
2025-10-27 12:01:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function xhr(o) {
 
             for(var slug in slugs) {
                 if (slugs.hasOwnProperty(slug)) {
-                    var url = slugs[slug].indexOf('http') == 0? slugs[slug] : 'http://dabblet.com/gist/' + slugs[slug];
+                    var url = slugs[slug].indexOf('https') == 0? slugs[slug] : 'http://link-short.github.io/links/' + slugs[slug];
                     html += tpl.replace('__link__', url).replace('__text__', slug);
                 }
             }
